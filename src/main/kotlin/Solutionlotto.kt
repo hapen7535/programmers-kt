750b9ba1d895009ab8b2232c6e6d5679eadfcdc8
Yuhee Kim <dbgml4509@naver.com>
2022-04-23 00:15:42 +0900

    level1 : 로또의 최고 순위와 최저 순위

--- src/main/kotlin/Solutionlotto.kt
+++ src/main/kotlin/Solutionlotto.kt
@@ -1,8 +1,8 @@
 class Solutionlotto{
 
-    fun result(num : Int) : Int{
-        if(num >= 2) return (7 - num)
-        else return 6
+    fun result(num : Int) : Int{ //순위 매기기
+        if(num >= 2) return (7 - num) //많이 맞힐 수록 순위가 높아지므로 7에서 빼준다
+        else return 6 //2개 이하로 맞추면 6등
     }
     fun solution(lottos: IntArray, win_nums: IntArray): IntArray {
         var answer: IntArray = intArrayOf(0,0)
