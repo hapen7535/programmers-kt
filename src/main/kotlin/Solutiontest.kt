d43ed4027689e436704140bfeadfa066283f3440
Yuhee Kim <dbgml4509@naver.com>
2022-05-05 22:03:10 +0900

    level1 : 체육복 답안 수정

--- src/main/kotlin/Solutiontest.kt
+++ src/main/kotlin/Solutiontest.kt
@@ -13,16 +13,16 @@ class Solutiontest  {
 
         var correct_cnt = arrayOf(0,0,0) //맞힌 개수 리스트
 
-        for(i in 0 answers.indices){
+        for(i in answers.indices){
             if(answers[i] == list1[i%5]) correct_cnt[0] = correct_cnt[0] + 1 //1번 수포자가 맞혔을 때
             if(answers[i] == list1[i%8]) correct_cnt[1] = correct_cnt[1] + 1 //2번 수포자가 맞혔을 때
             if(answers[i] == list1[i%10]) correct_cnt[2] = correct_cnt[2] + 1 //3번 수포자가 맞혔을 때
         }
         
-        val max = correct_cnt.max() //1,2,3번 수포자 중에서 가장 많이 맞힌 사람
+        var max = correct_cnt.maxOrNull() //1,2,3번 수포자 중에서 가장 많이 맞힌 사람
         
         for(i in correct_cnt.indices){ 
-            if(correct_cnt[i]>=max) answer.add(i+1) //가장 많이 맞힌 사람 답에 추가, 여러 명이면 오름차순으로 추가
+            if(correct_cnt[i] >= max!!) answer.add(i+1) //가장 많이 맞힌 사람 답에 추가, 여러 명이면 오름차순으로 추가
         }
 
 
--- src/main/kotlin/Solutiontrain.kt
+++ src/main/kotlin/Solutiontrain.kt
@@ -5,24 +5,35 @@ class Solutiontrain {
  fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
         var answer = n - lost.size
         var reserveList = ArrayList<Int>()
+
+        var sortedreserve = reserve.sortedArray().toList()
+        var sortedlost = lost.sortedArray().toList()
+        val union = sortedlost + sortedreserve
+
+        val intersection = union.groupBy { it }.filter{ it.value.size > 1 }.flatMap { it.value }.distinct()
+        sortedreserve = sortedreserve - intersection
+        sortedlost = sortedlost - intersection
+
         // 예외 처리 필요
         // 테스트 케이스 3,7,12,13,14 실패
         // 1. lost와 reserve가 정렬이 되어 있어야 함(lost와 reserve는 정렬이 된 상태로 들어오는 것이 아님)
-        // 2. 여분의 체육복을 가져온 학생이 체육복을 도난당하는 경우를 생각해야 함
+        // 2. 여분의 체육복을 가져온 학생이 체육복을 도난당하는 경우를 생각해야 함 -> lost와 reserve 공통 부분을 제거
+
+
         for(i in 0 until reserve.size){
-            reserveList.add(reserve[i])
+            reserveList.add(sortedreserve[i])
         }
 
         for(i in 0 until lost.size){
 
-            if(reserveList.contains(lost[i]-1)){
+            if(reserveList.contains(sortedlost[i]-1)){
                 answer = answer + 1
-                reserveList.remove(lost[i]-1)
+                reserveList.remove(sortedlost[i]-1)
                 continue
             }
-            if(reserveList.contains(lost[i]+1)){
+            if(reserveList.contains(sortedlost[i]+1)){
                 answer= answer + 1
-                reserveList.remove(lost[i]+1)
+                reserveList.remove(sortedlost[i]+1)
                 continue
             }
 
